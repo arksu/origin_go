5ee9fe4a10f5138c39772446f0a01ee4b2ac0e71
arksu <ark@ark.su>
2026-01-16 18:46:43 +0500

    opt

--- internal/config/config.go
+++ internal/config/config.go
@@ -136,7 +136,7 @@ func setDefaults(v *viper.Viper) {
 	v.SetDefault("game.chunk_lru_capacity", 1000)
 	v.SetDefault("game.chunk_lru_ttl", 20)
 	v.SetDefault("game.max_entities", 1048576)
-	v.SetDefault("game.worker_pool_size", 4)
+	v.SetDefault("game.worker_pool_size", 16)
 	v.SetDefault("game.load_workers", 16)
 	v.SetDefault("game.save_workers", 16)
 	v.SetDefault("game.spawn_timeout", 30*time.Second)
