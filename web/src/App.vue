6fc84db214e3e5c81a0bf168f34a250801f36e4f
arksu <ark@ark.su>
2026-02-03 01:07:32 +0500

    mv web tech

--- web/src/App.vue
+++
@@ -1,8 +0,0 @@
-<template>
-  <div class="min-h-screen bg-gray-900">
-    <router-view />
-  </div>
-</template>
-
-<script setup>
-</script>
--- web/src/config/network.js
+++
@@ -1,5 +0,0 @@
-export const networkConfig = {
-  wsUrl: 'ws://localhost:8080/ws',
-  clientVersion: '4.0.0',
-  pingIntervalMs: 10000
-}
